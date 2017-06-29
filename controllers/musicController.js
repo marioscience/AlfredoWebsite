f04dfc41f3acfd5fcfec8913ea8f4c4f8ea835c0
marioscience <marioscience@hotmail.com>
2017-06-29 13:59:12 -0400

    Created API for music section

---
+++ controllers/musicController.js
@@ -0,0 +1,65 @@
+var Music = require("../models/musicModel");
+
+module.exports = function(app) {
+    app.get("/api/music/playlist", function(req, res) {
+        Music.findOne({}, function(err, result) {
+            console.log(result.musicPlaylist);
+            res.send(result.musicPlaylist);
+        });
+    });
+
+    app.get("/api/music", function(req, res) {
+        Music.findOne({}, function(err, result) {
+            res.send(result);
+        });
+    });
+
+    // This request will receive a file later too
+    app.put("/api/music/video", function(req, res) {
+        Music.findByIdAndUpdate(req.body.id, {
+            $set: { videoUrl: req.body.videoUrl }
+        }, function(err) {
+            if(err) throw err;
+
+            res.send("Video updated succesfully");
+        });
+    });
+
+    app.post("/api/music/song", function(req, res) {
+        Music.findOne({}, function(err, music) {
+            music.musicPlaylist.push({
+                songName: req.body.songName,
+                artist: req.body.artist,
+                songUrl: req.body.songUrl
+            });
+
+            music.save(function(err) {
+                if(err) throw err;
+            });
+
+            res.send("Song added successfully!");
+        });
+    });
+
+    app.put("/api/music/song", function(req, res) {
+        Music.findOneAndUpdate({ "musicPlaylist._id": req.body.id }, {
+            "musicPlaylist.$.songName": req.body.songName,
+            "musicPlaylist.$.artist": req.body.artist,
+            "musicPlaylist.$.songUrl": req.body.songUrl
+        }, function(err) {
+            if(err) throw err;
+            res.send("Song updated successfully");
+        });
+    });
+
+    app.delete("/api/music/song", function(req, res) {
+        Music.findOneAndUpdate({}, {
+            $pull: {
+                "musicPlaylist": { "_id": req.body.id }
+            }
+        },function(err) {
+            if(err) throw err;
+            res.send("Song deleted successfully");
+        });
+    });
+};
