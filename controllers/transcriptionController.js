d93f02a078e3022d0ea0c9acb81a3b83a45a30b4
marioscience <marioscience@hotmail.com>
2017-06-29 14:39:16 -0400

    Created API for transcription section

---
+++ controllers/transcriptionController.js
@@ -0,0 +1,57 @@
+var Transcription = require("../models/transcriptionModel");
+
+module.exports = function(app) {
+    app.get("/api/transcription", function(req, res) {
+        Transcription.find({}, function(err, transcription) {
+            if(err) throw err;
+            res.send(transcription);
+        });
+    });
+
+    app.put("/api/transcription/description", function(req, res) {
+        Transcription.findOneAndUpdate({}, {
+            $set: { description: req.body.description }
+        }, function(err) {
+            if(err) throw err;
+            res.send("Transcription description updated succesfully.");
+        })
+    });
+
+    app.post("/api/transcription", function(req, res) {
+        Transcription.findOne({}, function(err, transcription) {
+            transcription.transcriptions.push({
+                title: req.body.title,
+                videoUrl: req.body.videoUrl,
+                description: req.body.description,
+                pdfUrl: req.body.pdfUrl
+            });
+
+            transcription.save(function(err) {
+                if(err) throw err;
+            });
+
+            res.send("Transcription added successfully.");
+        });
+    });
+
+    app.put("/api/transcription", function(req, res) {
+        Transcription.findOneAndUpdate({ "transcriptions._id": req.body.id }, {
+            "transcriptions.$.title": req.body.title,
+            "transcriptions.$.videoUrl": req.body.videoUrl,
+            "transcriptions.$.description": req.body.description,
+            "transcriptions.$.pdfUrl": req.body.pdfUrl
+        }, function(err) {
+            if(err) throw err;
+            res.send("Transcription modified successfully.");
+        });
+    });
+
+    app.delete("/api/transcription", function(req, res) {
+        Transcription.findOneAndUpdate({}, {
+            $pull: { "transcriptions": { "_id": req.body.id }}
+        }, function(err) {
+            if(err) throw err;
+            res.send("Transcription succesfully deleted.");
+        });
+    });
+};
