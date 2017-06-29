86409d9f4655890017722e3bf1dedece0f47afa0
marioscience <marioscience@hotmail.com>
2017-06-29 14:50:49 -0400

    Created API for music section

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 var express = require("express");
 var config = require("./config");
 var mongoose = require("mongoose");
+var bodyParser = require("body-parser");
 
+//controllers
 var setupData = require("./controllers/setupController");
+var musicController = require("./controllers/musicController");
+var transcriptionController = require("./controllers/transcriptionController");
 
 var app = express();
 
@@ -12,8 +16,12 @@ var appEnv = process.env.NODE_ENV || "development";
 
 app.use(config.filePaths.client, express.static(__dirname + config.filePaths.server));
 app.set("view engine", "pug");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 mongoose.connect(config.getDBConnectionString(appEnv));
 setupData(app);
+musicController(app);
+transcriptionController(app);
 
 app.listen(port);
