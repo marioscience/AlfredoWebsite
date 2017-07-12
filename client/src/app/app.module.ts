10b71bb866c96bc137d03341fb582ae8b300f518
marioscience <marioscience@hotmail.com>
2017-07-12 03:19:30 -0400

    Added router module and main app settings

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {AppRoutingModule} from "./app-routing.module";
+import {HttpModule} from "@angular/http";
 
 import { AppComponent } from './app.component';
 
+import {IntroductionService} from "./services/introduction.service";
+
+import {HomeComponent} from "./home/home.component";
+import {IntroductionComponent} from "./home/introduction/introduction.component";
+import {MusicComponent} from "./home/music/music.component";
+import {TranscriptionComponent} from "./home/transcription/transcription.component";
+import {BiographyComponent} from "./home/biography/biography.component";
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    IntroductionComponent,
+    MusicComponent,
+    TranscriptionComponent,
+    BiographyComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [IntroductionService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
