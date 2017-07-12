84ec19883f8077269dab9e076d27bd3e463fb319
marioscience <marioscience@hotmail.com>
2017-07-12 03:18:09 -0400

    Added component files for admin page

---
+++ client/src/app/admin/biography/biography-edit.component.ts
@@ -0,0 +1,9 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: "biography-edit-section",
+  template: `<h1> This is the biography edit. put something here come on man</h1>`
+})
+export class BiographyEditComponent {
+
+}
---
+++ client/src/app/admin/home/home-edit.component.ts
@@ -0,0 +1,9 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: "introduction-section",
+  template: `<h1> This is the introduction section. put something here come on man</h1>`
+})
+export class IntroductionComponent {
+
+}
