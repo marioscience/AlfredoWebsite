dab4159cc1af42dfeed51ef6e7db454ce3e3bb60
marioscience <marioscience@hotmail.com>
2017-07-17 22:05:30 -0400

    I'm not sure how I can be this stupid sometimes...

--- controllers/setupController.js
+++ controllers/setupController.js
@@ -41,45 +41,93 @@ var seedData = {
         ]
     },
     transcription: {
-        description: "transcriptions are a way to translate a dominican rythm into other superior rytms like jazz and stuff",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vel risus hendrerit, dapibus dolor at, placerat arcu. Nullam ut\n" +
+        "mauris ac augue ultrices sollicitudin quis sed neque. Proin feugiat vehicula volutpat",
         transcriptions: [
             {
-                title: "Gaga to Jazz",
+                title: "Transcription #1 Title",
                 videoUrl: "/videos/gagatojazz.mp4",
-                description: "This is how I converted that shit into other shit",
+                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus et nulla ut odio commodo suscipit. Curabitur vel convallis quam, et dapibus sem. Curabitur nec molestie odio. Donec nulla sapien, venenatis ut nisl sed, gravida semper ipsum. Fusce rhoncus purus nisl, ut interdum nisi lacinia at. Quisque ut enim massa. Aenean vel vulputate orci, et iaculis turpis. Aenean tincidunt, lectus eu malesuada scelerisque, turpis eros bibendum tellus, nec placerat lorem lacus non risus. Suspendisse sit amet.",
                 pdfUrl: "/pdfs/gagatojazz.pdf"
             },
             {
-                title: "Palo to Blues",
+                title: "Transcription #2 Title",
                 videoUrl: "/videos/palotoblues.mp4",
-                description: "This is how I converted that shit into that other shit",
+                description: "Cras odio quam, scelerisque et finibus et, viverra ut magna. Nullam a justo sit amet est sagittis tincidunt nec ullamcorper ante. Pellentesque eget egestas arcu, ut commodo mauris. Nulla facilisi. Phasellus aliquet ligula in ligula tempor porta. Duis sed dignissim enim. Aliquam sodales, sapien vel accumsan porttitor, mauris ex dapibus dui, a rutrum diam lacus sit amet mauris. Vestibulum mattis rhoncus aliquet. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sagittis orci urna, et.",
                 pdfUrl: "/pdfs/palotoblues.pdf"
             }
         ]
     },
     biography: {
-        biographyText: "I am the musician that plays the guitar and all that. I know I'm good because the guitar makes cool sounds.",
+        biographyText: "Alfredo Balcacer (b. March 11, 1984) is a guitarist and educator from the Dominican Republic. He graduated with a degree in\n" +
+        "Music Performance from Utah State University, and is currently finishing his masters degree in Music Performance at Western\n" +
+        "Michigan University. After coming to the United States, he has studied privately with Corey and Mike Christiansen, Fareed\n" +
+        "Haque, Tom Knific, Pat Martino, Bryan Baker, Fred Hamilton, Ed Simon, among others. Alfredo has performed in USA, Brazil,\n" +
+        "Canada and the Dominican Republic, and taught guitar privately for 8 years back home. Besides being a former member of the\n" +
+        "award-winning groups such a Gold Company, USU Jazz Orchestra, WMU Jazz Orchestra, Mas Que Nada, Alfredo is also a recipient\n" +
+        "of scholarships by the Dominican Republic Government, Utah State Caine College of the Arts and WMU Department of\n" +
+        "Music.\n" +
+        "For the past 10 years, Alfredo has performed and collaborated with many artists such as Peter Erskine, Randy Brecker, Corey\n" +
+        "Christiansen, George Garzone, Peter Eldridge, Cedric Dent, Jeremy Siskind, Deborah Brown, among others. As attendee to\n" +
+        "many masterclasses, he has performed with John Scofield, Chico Pinheiro, Julian Lage, Peter Bernstein, and Philip Catherine. In\n" +
+        "the Dominican Republic, he shared stage and recorded with Los Hermanos Rosario, Vakero, Javier Vargas, Cuquito Moré,\n" +
+        "Otoniel Nicolas, Guy Frometa, Juan Francisco Ordóñez, Josean Jacobo, Esar Simo, Los Rayo Solares, Diego Mena, El Metro, and\n" +
+        "many, many others.",
         picUrl: "/images/biograpypic.jpg",
         gear: [
             {
                 gearType: "Guitars",
                 items: [
                     {
-                        name: "Strocaster"
+                        name: "Carvin HF2 (Allan Holdsworth Signature\n" +
+                        "Model)"
                     },
                     {
-                        name: "Whatever"
+                        name: "Ibanez Artist 1978"
+                    },
+                    {
+                        name: "Fender Telecaster Custom (w/Joe"
                     }
                 ]
             },
             {
-                gearType: "Pedals",
+                gearType: "Amps",
                 items: [
                     {
-                        name: "Distolchon"
+                        name: "Tech 21 Trademark 60"
                     },
                     {
-                        name: "CoolZoundz 3000"
+                        name: "Hot Rod Deluxe w/mod."
+                    }
+                ]
+            },
+            {
+                gearType: "Pedals, effects and accessories",
+                items: [
+                    {
+                        name: "Line 6 Helix LT"
+                    },
+                    {
+                        name: "Xotic RC Booster"
+                    },
+                    {
+                        name: "Ernie Ball Volume Pedal"
+                    },
+                    {
+                        name: "EWS Subtle Volume Pedal"
+                    }
+                ]
+            },
+            {
+                gearType: "Strings / Pick",
+                items: [
+                    {
+                        name: "On Carvin HF2 (Thomastick, strings): 0.11, 0.14, 0.16, 0.28, 0.37, 0.48"
+                    },
+                    {
+                        name: "On Fender Telecaster:\n" +
+                        "D’addario stock set of 11’s\n" +
+                        "Dunlop Gator H3 picks"
                     }
                 ]
             }
@@ -90,6 +138,8 @@ var seedData = {
 
 // I guess you need to put a file manually for things requiring files. Maybe there's a way to automate this as well, but
 // for now we'll just seed the path to mongo and render the file that was put manually in public from there.
+
+/* TODO: v ADD FOR LOOPS FOR OBJECTS. YOUR A PROGRAMMER FOR CHRIST SAKES! v */
 module.exports = function(app) {
 
     var logResult = function(err, result) {
@@ -100,84 +150,10 @@ module.exports = function(app) {
 
     app.get("/api/appSetup", function(req, res) {
         /* Add seed data */
-        Introduction.create({
-            title: seedData.introduction.title,
-            description: seedData.introduction.description,
-            introductionPicUrl: seedData.introduction.introductionPicUrl
-        }, logResult);
-
-        Music.create({
-            videoUrl: seedData.music.videoUrl,
-            musicPlaylist: [
-                {
-                    songName: seedData.music.musicPlaylist[0].songName,
-                    artist: seedData.music.musicPlaylist[0].artist,
-                    songUrl: seedData.music.musicPlaylist[0].songUrl
-                },
-                {
-                    songName: seedData.music.musicPlaylist[1].songName,
-                    artist: seedData.music.musicPlaylist[1].artist,
-                    songUrl: seedData.music.musicPlaylist[1].songUrl
-                },
-                {
-                    songName: seedData.music.musicPlaylist[2].songName,
-                    artist: seedData.music.musicPlaylist[2].artist,
-                    songUrl: seedData.music.musicPlaylist[2].songUrl
-                },
-                {
-                    songName: seedData.music.musicPlaylist[3].songName,
-                    artist: seedData.music.musicPlaylist[3].artist,
-                    songUrl: seedData.music.musicPlaylist[3].songUrl
-                }
-            ]
-        }, logResult);
-
-        Transcription.create({
-            description: seedData.transcription.description,
-            transcriptions: [
-                {
-                    title: seedData.transcription.transcriptions[0].title,
-                    videoUrl: seedData.transcription.transcriptions[0].videoUrl,
-                    description: seedData.transcription.transcriptions[0].description,
-                    pdfUrl: seedData.transcription.transcriptions[0].pdfUrl
-                },
-                {
-                    title: seedData.transcription.transcriptions[1].title,
-                    videoUrl: seedData.transcription.transcriptions[1].videoUrl,
-                    description: seedData.transcription.transcriptions[1].description,
-                    pdfUrl: seedData.transcription.transcriptions[1].pdfUrl
-                }
-            ]
-        }, logResult);
-
-        Biography.create({
-            biographyText: seedData.biography.biographyText,
-            picUrl: seedData.biography.picUrl,
-            gear: [
-                {
-                    gearType: seedData.biography.gear[0].gearType,
-                    items: [
-                        {
-                            name: seedData.biography.gear[0].items[0].name
-                        },
-                        {
-                            name: seedData.biography.gear[0].items[1].name
-                        }
-                    ]
-                },
-                {
-                    gearType: seedData.biography.gear[1].gearType,
-                    items: [
-                        {
-                            name: seedData.biography.gear[1].items[0].name
-                        },
-                        {
-                            name: seedData.biography.gear[1].items[1].name
-                        }
-                    ]
-                }
-            ]
-        }, logResult);
+        Introduction.create(seedData.introduction, logResult);
+        Music.create(seedData.music, logResult);
+        Transcription.create(seedData.transcription, logResult);
+        Biography.create(seedData.biography, logResult);
 
         res.send("App Data Seeded Successfully");
 
