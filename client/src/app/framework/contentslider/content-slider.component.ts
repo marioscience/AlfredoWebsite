9e08b95cc99f866ea67b230d7bb349bd87bc0881
marioscience <marioscience@hotmail.com>
2017-07-20 21:30:04 -0400

    Content slider styling and dynamic content

--- client/src/app/framework/contentslider/content-slider.component.ts
+++ client/src/app/framework/contentslider/content-slider.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit, OnDestroy, ElementRef} from "@angular/core";
+import {Component, Input, AfterContentInit, OnDestroy, ElementRef} from "@angular/core";
 import * as $ from "jquery";
 import "slick-carousel";
 
@@ -7,13 +7,13 @@ import "slick-carousel";
   templateUrl: "./content-slider.component.html",
   styleUrls: ["./content-slider.component.scss"]
 })
-export class ContentSliderComponent implements OnInit, OnDestroy {
+export class ContentSliderComponent implements AfterContentInit, OnDestroy {
   @Input() public config: any; // there's probably a type for this in slick typings
 
   constructor(private el: ElementRef) {
   }
 
-  ngOnInit(): void {
+  ngAfterContentInit(): void {
     $(this.el.nativeElement).slick();
     console.log(this.el.nativeElement);
   }
