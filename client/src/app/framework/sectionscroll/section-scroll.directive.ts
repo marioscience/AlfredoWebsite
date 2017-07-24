a77c9d1f087a59d83d092b303e042ea2ab150bc6
marioscience <marioscience@hotmail.com>
2017-07-23 23:13:30 -0400

    Advanced functionality for navbar scroll

--- client/src/app/framework/sectionscroll/section-scroll.directive.ts
+++ client/src/app/framework/sectionscroll/section-scroll.directive.ts
@@ -2,7 +2,6 @@ import {Directive, Input, Inject, Renderer2, OnInit, ElementRef} from "@angular/
 import {SectionScrollService} from "./section-scroll.service";
 
 import {DOCUMENT} from "@angular/common";
-
 @Directive({
   selector: "[alfSectionScroll]"
 })
@@ -17,35 +16,65 @@ export class SectionScrollDirective implements OnInit {
   }
 
   ngOnInit(): void {
-    this.handleScrollTo();
-    this.handleScrollTarget();
+
+    if (this.scrollTarget) {
+      this.handleScrollTo();
+    }
+
+    if (this.scrollTo) {
+      this.handleScrollTarget();
+    }
   }
 
   private handleScrollTo(): void {
     /* Handle case when element is the target to be scrolled to */
-    if (this.scrollTarget) {
 
       /* observable that signals that an element should be scrolled to */
       this.sectionScrollService.getScrollToObservable()
         .subscribe((elementId) => {
-          console.log(elementId);
+          // if (elementId === this.scrollTarget) {
+          //   this.document.body.scrollTop = this.elem.nativeElement.getBoundingClientRect().top;
+          // }
         });
 
       /* When the page is scrolled detect if current section is the one selected */
       this.renderer.listen(this.document, "scroll", ($event) => {
-        console.log("scrolling...", $event.srcElement.body.scrollTop);
-        console.log("windowSize: ", $event.srcElement.body)
+        let elementScrollTop = this.elem.nativeElement.getBoundingClientRect().top;
+        let elementScrollBottom = this.elem.nativeElement.getBoundingClientRect().bottom;
+
+        if (elementScrollTop < 0 && elementScrollBottom > 0) {
+          this.sectionScrollService.targetScrolled(this.scrollTarget)
+        }
       });
-    }
   }
 
   private handleScrollTarget(): void {
     /* Handle case when element points to element to scroll to */
-    if (this.scrollTo) {
-      /* When an element is clicked that has scrollTo send the element identifier in the service */
+    /* When an element is clicked that has scrollTo send the element identifier in the service */
       this.renderer.listen(this.elem.nativeElement, "click", () => {
         this.sectionScrollService.scrollToTarget(this.scrollTo);
       });
+
+    /* this subcriptions signals that the navbar button should be highlighted */
+    this.sectionScrollService.getTargetScrolled()
+      .subscribe((target) => {
+        console.log(target);
+        console.log("top: ", this.findElementPosition(this.elem));
+      });
+  }
+
+  private findElementPosition(elem: ElementRef): number {
+    let node = elem.nativeElement;
+    let curtop = 0;
+    let curtopScroll = 0;
+
+    if (node.offsetParent) {
+      do {
+        curtop += node.offsetTop;
+        curtopScroll += node.offsetParent ? node.offsetParent.scrollTop : 0;
+      } while (node = node.offsetParent);
+
+      return curtop - curtopScroll;
     }
   }
 }
