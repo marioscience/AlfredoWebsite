17efc50c84c3fa2b6f80737b46011e1bdbb4202f
marioscience <marioscience@hotmail.com>
2017-08-03 19:45:16 -0400

    Added template and style files to concerts component

--- client/src/app/concerts/concerts.component.ts
+++ client/src/app/concerts/concerts.component.ts
@@ -2,13 +2,8 @@ import {Component} from "@angular/core";
 
 @Component({
   selector: "concerts",
-  template: `
-    <md-sidenav-container>
-      <h1 class="mat-h1">Concerts</h1>
-      <p>Concerts table</p>
-    </md-sidenav-container>
-  `
+  templateUrl: "./concerts.component.html",
+  styleUrls: ["./concerts.component.scss"]
 })
 export class ConcertsComponent {
-
 }
