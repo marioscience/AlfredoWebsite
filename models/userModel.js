6616343b9cee354eeaa4034e11d2d2afed53eade
marioscience <marioscience@hotmail.com>
2017-08-06 19:45:28 -0400

    Added user model for auth

---
+++ models/userModel.js
@@ -0,0 +1,10 @@
+var mongoose = require("mongoose");
+
+var userSchema = new mongoose.Schema({
+    user: "String",
+    password: "String"
+});
+
+var User = mongoose.model("User", userSchema);
+
+module.exports = User;
