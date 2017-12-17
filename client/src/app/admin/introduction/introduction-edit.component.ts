7db946add7082944de98064c03e704fdb337adb7
marioscience <marioscience@hotmail.com>
2017-12-17 17:14:58 -0400

    Modified material component tags

--- client/src/app/admin/introduction/introduction-edit.component.ts
+++ client/src/app/admin/introduction/introduction-edit.component.ts
@@ -4,12 +4,7 @@ import {Component} from "@angular/core";
   selector: "introduction-edit",
   template: `
     <h1>Introduction</h1>
-    <mat-input-container>
-      <input type="text" matInput placeholder="Title">
-    </mat-input-container>
-    <mat-input-container>
-      <<textarea name="" id="" cols="30" rows="10"></textarea>>
-    </mat-input-container>
+    <p>Introduction here</p>
   `
 })
 export class IntroductionEditComponent {
