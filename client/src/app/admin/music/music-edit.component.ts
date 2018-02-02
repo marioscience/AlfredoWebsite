fd867daac5313b6485578fcd90c6a445c6ada4e5
marioscience <marioscience@hotmail.com>
2018-02-02 17:02:52 -0400

    Added functionality in form to update music section

--- client/src/app/admin/music/music-edit.component.ts
+++ client/src/app/admin/music/music-edit.component.ts
@@ -1,12 +1,62 @@
 import {Component} from "@angular/core";
+import {Music, Song} from "../../models/music.model";
+import {MusicService} from "../../services/music.service";
+import {MatSnackBar} from "@angular/material";
 
 @Component({
   selector: "music-edit",
-  template: `
-    <h1>Music</h1>
-    <p> This is the music. put something here come on man.</p>
-  `
+  templateUrl: "./music-edit.component.html",
+  styleUrls: ["./music-edit.component.scss"]
 })
 export class MusicEditComponent {
+
+  music = new Music();
+  editMode: boolean = false;
+  songToEdit: Song;
+  addMode: boolean = false;
+  newSong: Song = new Song();
+
+  constructor(private musicService: MusicService, private snackBar: MatSnackBar) {
+    this.musicService.getMusic()
+      .then((data) => {
+        this.music = data;
+      });
+  }
+
+  editSong(song): void {
+    this.editMode = true;
+    this.songToEdit = Object.assign({}, song);
+  }
+
+  cancelSongEdit(): void {
+    this.editMode = false;
+  }
+
+  addNewSong(): void {
+    this.addMode = true;
+  }
+
+  cancelSongAdd(): void {
+    this.addMode = false;
+  }
+
+  saveVideo(): void {
+    this.musicService.saveVideo(this.music)
+      .then(() => {
+        this.snackBar.open("Video URL Updated successfully", "", {duration: 2000});
+      })
+      .catch(() => {
+        this.snackBar.open("Video URL Updated successfully", "!!!", {duration: 2000});
+      });
+
+  }
+
+  saveEdit(): void {
+
+  }
+
+  saveNewSong(): void {
+
+  }
 
 }
